419557bc8397465beee727cedf9d1bcc7fc70202
Siiha <90766306+Siiha@users.noreply.github.com>
2023-06-30 00:28:19 +0300

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,26 +1,23 @@
 import PySimpleGUI as sg
 from random import randint
-
-dice = lambda: randint(1, 6)
+dice = lambda: randint(1,6)
 graph = sg.Graph(
-    canvas_size=(512, 512),
-    graph_bottom_left=(0, 0),
+    canvas_size=(512,512),
+    graph_bottom_left=(0,0),
     graph_top_right=(512, 512),
-    drag_submits=True,
-    enable_events=True,
-    key="dice",
-)
-layout = [[graph], [sg.Button("roll")]]
-window = sg.Window("Dice", layout, finalize=True)
-picture = graph.draw_image(filename=f"inverted-dice-{dice()}.png", location=(0, 512))
+   drag_submits=True,
+    key='dice')
+layout = [
+   [graph],
+   [sg.Button("roll")]]
+window = sg.Window('Dice', layout, finalize=True)
+pic = lambda: graph.draw_image(filename=f"inverted-dice-{dice()}.png",location=(0, 512))
+picture = pic()
 while True:
-    event, values = window.read()
-    print(event)
-    if event == "roll":
-        window["dice"].delete_figure(picture)
-        picture = graph.draw_image(
-            filename=f"inverted-dice-{dice()}.png", location=(0, 512)
-        )
-    if event in (None, "Exit"):
-        break
+   event, values = window.read()
+   if event == 'roll':
+	   graph.delete_figure(picture)
+	   picture = pic()
+   if event in (None, 'Exit'):
+      break
 window.close()
